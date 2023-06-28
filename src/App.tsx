b46afd470d4e0e8a71a592c7b2b579eb9a942882
bamiyamy <bamiyamy@gmail.com>
2023-06-28 17:20:15 +0900

    react-router-dom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import Main from "./Page/Main/Main"
 import Header from "./Component/Header/Header"
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 
-function App() {
+const MainLayout = () => {
   return (
-    <div className="App">
-          <Header/>
-          <Main/>
-    </div>
-  );
+    <>
+      <Header/>
+      <Outlet/>
+    </>
+  )
 }
 
+const App = () => {
+  return(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route element={<MainLayout/>}>
+          <Route path="/" element={<Main/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 export default App;
